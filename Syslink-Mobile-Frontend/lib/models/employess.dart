2698b7c9f7c9e89ea0527fc57c4b4329fe3283eb
supanidaa <pear0442@gmail.com>
2020-06-28 22:10:38 +0700

    fix model

--- Syslink-Mobile-Frontend/lib/models/employess.dart
+++ Syslink-Mobile-Frontend/lib/models/employess.dart
@@ -1,13 +1,27 @@
-class Employess{
+class Employess {
   final String id;
   final String name;
   final String surname;
   final String username;
   final String password;
   final String department;
-  final String profile_img;
-  final String ref_id;
-  final String role_id;
+  final String profileimg;
+  final String refid;
+  final String roleid;
+  final String createdat;
+  final String updatedat;
 
-  Employess(this.id, this.name, this.surname, this.username, this.password, this.department, this.profile_img, this.ref_id, this.role_id);
+  Employess({
+      this.id,
+      this.name,
+      this.surname,
+      this.username,
+      this.password,
+      this.department,
+      this.profileimg,
+      this.refid,
+      this.roleid,
+      this.createdat,
+      this.updatedat
+      });
 }
